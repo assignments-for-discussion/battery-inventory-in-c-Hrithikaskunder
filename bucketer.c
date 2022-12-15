4a944faa39141de83eced596ca484afdc9152d12
Hrithikaskunder <88131615+Hrithikaskunder@users.noreply.github.com>
2022-12-15 17:06:05 +0530

    Update bucketer.c

--- bucketer.c
+++ bucketer.c
@@ -17,8 +17,6 @@ void testBucketingByNumberOfCycles() {
   const int numberOfBatteries = sizeof(chargeCycleCounts) / sizeof(chargeCycleCounts[0]);
   printf("Counting batteries by usage cycles...\n");
   
-    
-  }
   struct CountsByUsage counts = countBatteriesByUsage(chargeCycleCounts, numberOfBatteries);
 //   assert(counts.lowCount == 2);
 //   assert(counts.mediumCount == 3);
@@ -42,12 +40,13 @@ for(i=0;i<sizeof(chargeCycleCounts);i++){
     
     else {
     print("invalid charge cycle counts");
+      i++;
     }
   printf("Done counting :)\n");
+}
 }
 
 int main() {
   testBucketingByNumberOfCycles();
-  
   return 0;
 }
